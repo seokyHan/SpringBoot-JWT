33653fe6b6eec49f0352e37b80e178b07222c9fd
seokyHan <sseok1979@naver.com>
2022-08-05 17:35:41 +0900

    first commit

---
+++ src/main/java/com/example/springJwt/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.springJwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+
+        return httpSecurity.build();
+    }
+
+}
